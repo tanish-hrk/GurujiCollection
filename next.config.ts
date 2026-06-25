c8cfb2de6663ac261a93ccfdbf7c6ba95eaea298
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2026-06-25 16:36:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "via.placeholder.com" },
